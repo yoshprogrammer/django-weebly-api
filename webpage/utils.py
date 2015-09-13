75bb4aca95c9436caded92ec61abdf26dc79d761
Thomas Kwon <tdkwon@gmail.com>
2015-09-13 13:59:40 -0700

    added tests for weebly posts and get

--- webpage/utils.py
+++ webpage/utils.py
@@ -8,27 +8,77 @@ API_KEY = '0513rf6d0om4y1vmbm2gazxqrcwaye38'
 API_SECRET = '8mzamuu3vpk9laud7a9e5uw0yobaywxozkhpdtgh6iscd15puk11jkwb8h714duy'
 
 
-def weebly_api(my_method, my_data, my_url):
+def weebly_post(my_url, my_data):
 
     # get url
-    post_url = 'https://api.weeblycloud.com/' + my_url
+    full_url = 'https://api.weeblycloud.com/' + my_url
 
     # get hash
-    post_content = my_method + '\n' + my_url + '\n' + json.dumps(my_data)
-    post_content = bytes(post_content).encode('utf-8')
-    post_secret = bytes(API_SECRET).encode('utf-8')
-    post_hmac = hmac.new(post_secret, post_content, digestmod=hashlib.sha256).hexdigest()
-    post_hash = base64.b64encode(post_hmac)
+    my_content = 'POST' + '\n' + my_url + '\n' + json.dumps(my_data)
+    my_hmac = hmac.new(API_SECRET, my_content, digestmod=hashlib.sha256).hexdigest()
+    my_hash = base64.b64encode(my_hmac)
 
     # get headers
     post_header = {
     'Content-Type': 'application/json',
     'X-Public-Key': API_KEY,
-    'X-Signed-Request-Hash': post_hash,
+    'X-Signed-Request-Hash': my_hash,
     }
 
     # send request
-    resp = requests.post(post_url, data=json.dumps(my_data), headers=post_header)
+    resp = requests.post(full_url, data=json.dumps(my_data), headers=post_header)
 
     return resp
+
+
+def weebly_get(my_url):
+
+    # get url
+    full_url = 'https://api.weeblycloud.com/' + my_url
+
+    # get hash
+    my_content = 'GET' + '\n' + my_url
+    my_hmac = hmac.new(API_SECRET, my_content, digestmod=hashlib.sha256).hexdigest()
+    my_hash = base64.b64encode(my_hmac)
+
+    # get headers
+    get_header = {
+    'X-Public-Key': API_KEY,
+    'X-Signed-Request-Hash': my_hash,
+    }
+
+    # send request
+    resp = requests.get(full_url, headers=get_header)
+
+    return resp
+
+if 0:
+    # test post to create user
+    my_url = 'user/'
+    my_data = {'email':'t9@you.com'}
+    resp = weebly_post(my_url, my_data)
+
+if 0:
+    # test post to create site for user
+    my_url = 'user/60598425/site'
+    my_data = {'domain': 'testsite231.com'}
+
+    resp = weebly_post(my_url, my_data)
+
+if 1:
+    # test to fetch user info
+    my_url = 'user/60598425'
+
+    resp = weebly_get(my_url)
+
+if 0:
+    # test to fetch sites for user. fail
+    my_url = 'user/60598425/site'
+
+    resp = weebly_get(my_url)
+
+if 1:
+    # print results
+    print(resp.status_code)
+    print(resp.text)
 
--- webpage/views.py
+++ webpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
+from django.shortcuts import get_object_or_404
 
 from .utils import *
 from .forms import *
@@ -23,7 +24,7 @@ class SignUpView(CreateView):
             }
 
         # send off to weebly for user_id
-        resp = weebly_api('POST', my_data, 'user/')
+        resp = weebly_api('POST', 'user/', my_data)
 
         if (resp.status_code == 200):
             form.instance.user_id = resp.json()['user']['user_id']
@@ -38,6 +39,25 @@ class SignUpView(CreateView):
 
 class PrincipalDetailView(DetailView):
     model = Principal
+
+    def get_context_data(self, **kwargs):
+        context = super(PrincipalDetailView, self).get_context_data(**kwargs)
+
+        #fetch sites from weebly
+        principal = get_object_or_404(Principal, pk=self.kwargs['pk'])
+        user_id = principal.user_id
+        my_url = 'user/' + str(user_id) + '/site'
+        resp = weebly_api('GET', 'my_url')
+
+        if (resp.status_code == 200):
+
+
+        else:
+            #TODO need error message
+            return redirect('principal.list.view')
+
+        return context
+
 
 
 class PrincipalListView(ListView):
