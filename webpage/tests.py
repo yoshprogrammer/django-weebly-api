85b3a59b185662a763c8264ad6581380cd7a5c78
Thomas Kwon <tdkwon@gmail.com>
2015-09-13 22:14:30 -0700

    all util methods working. Some tests fail still

--- webpage/tests.py
+++ webpage/tests.py
@@ -6,14 +6,102 @@ import string
 from .utils import *
 
 # Create your tests here.
-class WeeblyApiTest(TestCase):
+class WeeblyApiTests(TestCase):
 
     def setUp(self):
-        self.email = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6)) + '@test.com'
+        #self.email = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6)) + '@test.com'
+        self.email   = 'test231@yahoo.com'
+        self.user_id = '60521671'
+        self.site_id = '685054111468103842'
+        self.domain  = None
+
 
     def test_new_user_account_creation(self):
-        print(self.email)
-        self.assertEqual(True, True)
+        my_url = 'user/'
+        my_data = "{'email': " + "'" + self.email + "'}"
+        resp = weebly_post(my_url, my_data)
+        self.assertEqual(resp.status_code, 200)
+        self.user_id =  resp.json()['user']['user_id']
 
-    def test_new_user_site_creation(self):
-        self.assertEqual(True, False)
+
+    def test_user_site_creation(self):
+        my_url = 'user/' + self.user_id + '/site'
+        self.domain = self.user_id + '.com'
+        my_data = {'domain': self.domain}
+        resp = weebly_post(my_url, my_data)
+
+
+    def test_fetch_user_info(self):
+        my_url = 'user/' + self.user_id
+
+        resp = weebly_get(my_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['user']['email'], self.email)
+        self.assertEqual(resp.json()['user']['test_mode'], True)
+        self.assertEqual(resp.json()['user']['language'], 'en')
+
+
+    def test_change_user_email_test_mode(self):
+        my_url = 'user/' + self.user_id
+        my_data = {'email': 'me_updated@test.com', 'test_mode': True, 'language': 'it'}
+
+        resp = weebly_patch(my_url, my_data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['user']['email'], self.email)
+        self.assertEqual(resp.json()['user']['test_mode'], True)
+        self.assertEqual(resp.json()['user']['language'], 'it')
+
+
+    def test_disable_user(self):
+        my_url = 'user/' + self.user_id + '/disable'
+
+        resp = weebly_post(my_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['success'], True)
+
+
+    def test_enable_user(self):
+        my_url = 'user/' + self.user_id + '/enable'
+
+        resp = weebly_post(my_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['success'], True)
+
+
+    def test_create_site(self):
+        my_url = 'user/' + self.user_id + '/site'
+        my_data = "{'domain': '" + self.user_id +  \
+                  ".com', 'site_title': 'My Test Website'}"
+
+        resp = weebly_post(my_url, my_data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['site']['domain'], self.user_id + '.com')
+        self.assertEqual(resp.json()['site']['site_title'], 'My Test Website')
+        self.site_id = resp.json()['site']['site_id']
+
+
+    def test_delete_site(self):
+        my_url = 'user/' + self.user_id + '/site/' + self.site_id
+        resp = weebly_delete(my_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['success'], 'True')
+
+
+    def test_restore_site(self):
+        my_url = 'user/' + self.user_id + '/site/' + self.site_id + '/restore'
+        my_data = "{'domain': '" + self.domain + "'}"
+
+        resp = weebly_post(my_rul, my_data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['success'], 'True')
+
+
+    def test_fetch_user_sites(self):
+        my_url = 'user/' + self.user_id + '/site'
+
+        resp = weebly_get(my_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()['sites'][0]['domain'], self.domain)
+        self.assertEqual(resp.json()['sites'][0]['site_id'], self.site_id)
+
+
--- webpage/utils.py
+++ webpage/utils.py
@@ -1,32 +1,54 @@
+from django.conf import settings
+
 import requests
 import hashlib
 import hmac
 import json
 import base64
 
-API_KEY = '0513rf6d0om4y1vmbm2gazxqrcwaye38'
-API_SECRET = '8mzamuu3vpk9laud7a9e5uw0yobaywxozkhpdtgh6iscd15puk11jkwb8h714duy'
+API_KEY = settings.API_KEY
+API_SECRET =settings.API_SECRET
+base_url = 'https://api.weeblycloud.com/'
 
 
-def weebly_post(my_url, my_data):
-
-    # get url
-    full_url = 'https://api.weeblycloud.com/' + my_url
-
-    # get hash
-    my_content = 'POST' + '\n' + my_url + '\n' + json.dumps(my_data)
+def weebly_hash(my_content):
     my_hmac = hmac.new(API_SECRET, my_content, digestmod=hashlib.sha256).hexdigest()
     my_hash = base64.b64encode(my_hmac)
 
+    return my_hash
+
+
+def weebly_post(my_url, my_data=None):
+
+    # get url
+    full_url = base_url + my_url
+
+    # get hash
+    if (my_data == None):
+        my_content = 'POST' + '\n' + my_url + '\n'
+    else:
+        my_content = 'POST' + '\n' + my_url + '\n' + json.dumps(my_data)
+
+    my_hash = weebly_hash(my_content)
+
     # get headers
-    post_header = {
-    'Content-Type': 'application/json',
-    'X-Public-Key': API_KEY,
-    'X-Signed-Request-Hash': my_hash,
-    }
+    if (my_data == None):
+        post_header = {
+            'X-Public-Key': API_KEY,
+            'X-Signed-Request-Hash': my_hash,
+        }
+    else:
+        post_header = {
+            'Content-Type': 'application/json',
+            'X-Public-Key': API_KEY,
+            'X-Signed-Request-Hash': my_hash,
+        }
 
     # send request
-    resp = requests.post(full_url, data=json.dumps(my_data), headers=post_header)
+    if (my_data == None):
+        resp = requests.post(full_url, headers=post_header)
+    else:
+        resp = requests.post(full_url, data=json.dumps(my_data), headers=post_header)
 
     return resp
 
@@ -34,17 +56,16 @@ def weebly_post(my_url, my_data):
 def weebly_get(my_url):
 
     # get url
-    full_url = 'https://api.weeblycloud.com/' + my_url
+    full_url = base_url + my_url
 
     # get hash
-    my_content = 'GET' + '\n' + my_url
-    my_hmac = hmac.new(API_SECRET, my_content, digestmod=hashlib.sha256).hexdigest()
-    my_hash = base64.b64encode(my_hmac)
+    my_content = 'GET' + '\n' + my_url + '\n'
+    my_hash = weebly_hash(my_content)
 
     # get headers
     get_header = {
-    'X-Public-Key': API_KEY,
-    'X-Signed-Request-Hash': my_hash,
+        'X-Public-Key': API_KEY,
+        'X-Signed-Request-Hash': my_hash,
     }
 
     # send request
@@ -52,33 +73,43 @@ def weebly_get(my_url):
 
     return resp
 
-if 0:
-    # test post to create user
-    my_url = 'user/'
-    my_data = {'email':'t9@you.com'}
-    resp = weebly_post(my_url, my_data)
 
-if 0:
-    # test post to create site for user
-    my_url = 'user/60598425/site'
-    my_data = {'domain': 'testsite231.com'}
+def weebly_patch(my_url, my_data):
+    # get url
+    full_url = base_url + my_url
 
-    resp = weebly_post(my_url, my_data)
+    # get hash
+    my_content = 'PATCH' + '\n' + my_url + '\n' + json.dumps(my_data)
+    my_hash = weebly_hash(my_content)
 
-if 0:
-    # test to fetch user info
-    my_url = 'user/60598425'
+    # get headers
+    post_header = {
+        'Content-Type': 'application/json',
+        'X-Public-Key': API_KEY,
+        'X-Signed-Request-Hash': my_hash,
+    }
 
-    resp = weebly_get(my_url)
+    # send request
+    resp = requests.put(full_url, data=json.dumps(my_data), headers=post_header)
 
-if 0:
-    # test to fetch sites for user. fail
-    my_url = 'user/60598425/site'
+    return resp
 
-    resp = weebly_get(my_url)
 
-if 0:
-    # print results
-    print(resp.status_code)
-    print(resp.text)
+def weebly_delete(my_url):
+    full_url = base_url + my_url
+
+    #get hash
+    my_content = 'DELETE' + '\n' + my_url + '\n'
+    my_hash = weebly_hash(my_content)
+
+    # get headers
+    post_header = {
+        'X-Public-Key': API_KEY,
+        'X-Signed-Request-Hash': my_hash,
+    }
+
+    # send request
+    resp = requests.delete(full_url, headers=post_header)
+
+    return resp
 
